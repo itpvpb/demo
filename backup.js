151ed190ab65a28d7fa224524d5be1caa8837e02
itpvpb <itpvpb@163.com>
2016-12-15 14:22:10 +0800

    Create backup.js

---
+++ backup.js
@@ -0,0 +1,10 @@
+{
+"scheme":"https",
+"api":"user.auth204.xyz:21568",
+"auth":"user.auth203.xyz:22135",
+"siteurl":"https://a.tzqby.xyz:17869",
+"error_notify":false,
+"init_notify":false,
+"msg":"",
+"error_page":"a.tzqby.xyz:17869"
+}
