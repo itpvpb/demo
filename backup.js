74f453f0737cef5eabcd6c59a5d28431399409f2
itpvpb <itpvpb@163.com>
2017-01-09 17:39:13 +0800

    Update backup.js

--- backup.js
+++ backup.js
@@ -4,7 +4,7 @@
 "auth": "u10.auth209.xyz:22135",
 "siteurl": "https://a.tzqby.xyz:17869",
 "homepage1": "https://a.tzqby.xyz:17869",
-"homepage2": "https://a.tzqby.xyz:17869",
+"homepage2": "https://b.tzqby.xyz:17869",
 "openhome": false,
 "domains": "'google.co.jp','google.com','google.com.hk','googleusercontent.com','googleapis.com','gstatic.com','gmail.com','sstatic.net','s3.amazonaws.com','googlesyndication.com','doubleclick.net','googleadservices.com'",
 "error_notify": false,
