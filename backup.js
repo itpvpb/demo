9923a8b44359c7da65b3492b8ea571ac44d6b7fc
itpvpb <itpvpb@163.com>
2017-01-09 13:53:45 +0800

    Update backup.js

--- backup.js
+++ backup.js
@@ -1,10 +1,14 @@
 {
-"scheme":"https",
-"api":"u9.auth209.xyz:21568",
-"auth":"u10.auth209.xyz:22135",
-"siteurl":"https://a.tzqby.xyz:17869",
-"error_notify":false,
-"init_notify":false,
-"msg":"",
-"error_page":"a.tzqby.xyz:17869"
+"scheme": "https",
+"api": "u9.auth209.xyz:21568",
+"auth": "u10.auth209.xyz:22135",
+"siteurl": "https://a.tzqby.xyz:17869",
+"homepage1": "https://a.tzqby.xyz:17869",
+"homepage2": "https://a.tzqby.xyz:17869",
+"openhome": false,
+"domains": "'google.co.jp','google.com','google.com.hk','googleusercontent.com','googleapis.com','gstatic.com','gmail.com','sstatic.net','s3.amazonaws.com','googlesyndication.com','doubleclick.net','googleadservices.com'",
+"error_notify": false,
+"init_notify": false,
+"msg": "",
+"error_page": "a.tzqby.xyz:17869"
 }
