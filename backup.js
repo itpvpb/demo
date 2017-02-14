bfbf246c94d86a372e8d71388117865327e1521f
itpvpb <itpvpb@163.com>
2017-02-14 20:51:25 +0800

    Update backup.js

--- backup.js
+++ backup.js
@@ -3,7 +3,7 @@
 "api": "htauth1.tedada.pw",
 "auth": "htauth2.tedada.pw",
 "siteurl": "http://www.haitaojiasu.pw",
-"homepage": "https://a.tzqby.xyz:17869",
+"homepage": "http://www.baidu.com",
 "openhome": false,
 "domains": "'google.co.jp','google.com','google.com.hk','googleusercontent.com','googleapis.com','gstatic.com','gmail.com','sstatic.net','s3.amazonaws.com','googlesyndication.com','doubleclick.net','googleadservices.com'",
 "error_notify": false,
